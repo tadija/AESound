e056fae299069fe1b465be7a199d18e518e10b15
Marko Tadic <tadija@me.com>
2016-05-15 20:28:39 +0200

    Minor improvements

--- AESound/AESound.swift
+++ AESound/AESound.swift
@@ -30,8 +30,11 @@ public class AESound {
 
 public class AESoundTableViewController: UITableViewController {
     
-    var paths = [String]()
+    // MARK: - Properties
     
+    var urls = [NSURL]()
+    
+    /// - NOTE: This method will only work on device, not on simulator!
     func loadPaths() {
         let fileManager = NSFileManager.defaultManager()
         let keys = [NSURLIsDirectoryKey]
@@ -43,44 +46,42 @@ public class AESoundTableViewController: UITableViewController {
         
         for object in enumerator {
             guard let url = object as? NSURL else { return }
-            
             do {
                 var resource: AnyObject?
                 try url.getResourceValue(&resource, forKey: NSURLIsDirectoryKey)
                 guard let isDirectory = (resource as? NSNumber)?.boolValue else { return }
-                if !isDirectory {
-                    guard let path = url.path else { return }
-                    paths.append(path)
-                }
-            } catch {
-                print(error)
-            }
+                if !isDirectory { urls.append(url) }
+            } catch { print(error) }
         }
-        
     }
+    
+    // MARK: - Lifecycle
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
         loadPaths()
-        print(paths)
-        
-        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
+    // MARK: - UITableViewDataSource
+    
     public override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return paths.count
+        return urls.count
     }
     
     public override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        let path = paths[indexPath.row]
-        cell.textLabel?.text = path
+        let defaultCell = UITableViewCell(style: .Subtitle, reuseIdentifier: "Cell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") ?? defaultCell
+        
+        let url = urls[indexPath.row]
+        
+        cell.textLabel?.text = url.lastPathComponent
+        cell.detailTextLabel?.text = url.path
+        
         return cell
     }
     
     public override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let path = paths[indexPath.row]
+        guard let path = urls[indexPath.row].path else { return }
         AESound.play(path)
     }
     
