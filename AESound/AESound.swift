a77b27bfda99253adea760b9e66ed024e3255462
Marko Tadic <tadija@me.com>
2016-05-15 19:58:18 +0200

    Implemented initial logic for getting all paths of system sounds in AESoundTableViewController

--- AESound/AESound.swift
+++ AESound/AESound.swift
@@ -26,4 +26,44 @@ public class AESound {
     
 }
 
+// MARK: - AESoundTableViewController
 
+public class AESoundTableViewController: UITableViewController {
+    
+    var paths = [String]()
+    
+    func loadPaths() {
+        let fileManager = NSFileManager.defaultManager()
+        let keys = [NSURLIsDirectoryKey]
+        
+        guard let
+            url = NSURL(string: "/System/Library/Audio/UISounds"),
+            enumerator = fileManager.enumeratorAtURL(url, includingPropertiesForKeys: keys, options: [], errorHandler: nil)
+        else { return }
+        
+        for object in enumerator {
+            guard let url = object as? NSURL else { return }
+            
+            do {
+                var resource: AnyObject?
+                try url.getResourceValue(&resource, forKey: NSURLIsDirectoryKey)
+                guard let isDirectory = (resource as? NSNumber)?.boolValue else { return }
+                if !isDirectory {
+                    guard let path = url.path else { return }
+                    paths.append(path)
+                }
+            } catch {
+                print(error)
+            }
+        }
+        
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadPaths()
+        print(paths)
+    }
+    
+}
